8777b375079ed309b8510ae0fe7045987b701d8a
Jaskiran-k-ahuja <84671825+Jaskiran-k-ahuja@users.noreply.github.com>
2022-10-23 02:14:39 +0530

    manipulation with pandas series function for Single dimensional data

---
+++ pandas_project/population_data_manipulation/population.py
@@ -0,0 +1,5 @@
+import pandas as pd
+population=[1000,2000,3000,4000]
+countries=['India','China','England','Australia']
+s2=pd.Series(population,index=countries)
+print(s2)
---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+# print(np.__version__)
+# print(pd.__version__)
+# python_list= [1,2,3,4,5]
+# np_array_py=np.array(python_list)
+# print(type(np_array_py))
+# s=pd.Series([1,2,3,4,5],index=['a','b','c','d','e'])
+# print(s['b'])
+
